d6e919173326b1a30905c55dce2fba5516cb066a
HamdiMoadeb <hamdi.moadeb@tayara.tn>
2021-09-24 15:54:09 +0100

    create get all authors api client method

---
+++ lib/services/author_api_client.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:authers/models/author.dart';
+import 'package:authers/utils/constants.dart';
+import 'package:http/http.dart' as http;
+
+class AuthorApiClient {
+  Future<List<Author>> getAllAuthors() async {
+    List<Author> authors = [];
+
+    var url = Uri.parse('${Constants.BASE_URL}/authors');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var body = jsonDecode(response.body);
+
+      for (Map athr in body) {
+        authors.add(Author.fromJson(athr));
+      }
+    } else {
+      throw Exception('error getting authors');
+    }
+
+    return authors;
+  }
+}
